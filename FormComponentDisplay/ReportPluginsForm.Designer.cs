05da7427365e52ff9f0125d4cc175b1ffd2fadd7
DelphyAlmond <delphyalmond@gmail.com>
2025-12-04 00:30:01 +0400

    Unproven raw3, debug req

--- FormComponentDisplay/ReportPluginsForm.Designer.cs
+++ FormComponentDisplay/ReportPluginsForm.Designer.cs
@@ -28,24 +28,66 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-
-            this.Text = "< Плагины отчетов >";
-            this.Size = new Size(600, 400);
-            this.StartPosition = FormStartPosition.CenterParent;
-
-            var mainPanel = new FlowLayoutPanel
-            {
-                Dock = DockStyle.Fill,
-                FlowDirection = FlowDirection.TopDown,
-                WrapContents = false
-            };
-
-            this.Controls.Add(mainPanel);
+            mainFlowLayoutPanel = new FlowLayoutPanel();
+            statusStrip = new StatusStrip();
+            loadingLabel = new Label();
+            SuspendLayout();
+            // 
+            // mainFlowLayoutPanel
+            // 
+            mainFlowLayoutPanel.AutoScroll = true;
+            mainFlowLayoutPanel.Dock = DockStyle.Fill;
+            mainFlowLayoutPanel.FlowDirection = FlowDirection.TopDown;
+            mainFlowLayoutPanel.Location = new Point(0, 0);
+            mainFlowLayoutPanel.Name = "mainFlowLayoutPanel";
+            mainFlowLayoutPanel.Padding = new Padding(10);
+            mainFlowLayoutPanel.Size = new Size(600, 362);
+            mainFlowLayoutPanel.TabIndex = 0;
+            mainFlowLayoutPanel.WrapContents = false;
+            // 
+            // statusStrip
+            // 
+            statusStrip.ImageScalingSize = new Size(32, 32);
+            statusStrip.Location = new Point(0, 362);
+            statusStrip.Name = "statusStrip";
+            statusStrip.Size = new Size(600, 38);
+            statusStrip.TabIndex = 1;
+            statusStrip.Text = "statusStrip1";
+            // 
+            // loadingLabel
+            // 
+            loadingLabel.AutoSize = true;
+            loadingLabel.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            loadingLabel.Location = new Point(250, 180);
+            loadingLabel.Name = "loadingLabel";
+            loadingLabel.Size = new Size(175, 37);
+            loadingLabel.TabIndex = 2;
+            loadingLabel.Text = "Загрузка...";
+            loadingLabel.Visible = false;
+            // 
+            // ReportPluginsForm
+            // 
+            AutoScaleDimensions = new SizeF(15F, 30F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(600, 400);
+            Controls.Add(loadingLabel);
+            Controls.Add(mainFlowLayoutPanel);
+            Controls.Add(statusStrip);
+            Font = new Font("Microsoft Sans Serif", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            FormBorderStyle = FormBorderStyle.FixedDialog;
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "ReportPluginsForm";
+            StartPosition = FormStartPosition.CenterParent;
+            Text = "< Плагины отчетов >";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel mainFlowLayoutPanel;
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.Label loadingLabel;
     }
 }
